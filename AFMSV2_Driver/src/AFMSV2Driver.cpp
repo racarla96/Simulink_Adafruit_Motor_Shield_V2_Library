437a8f1efcb78cf2a0ecb00061d046f45b531035
racarla96 <racarla96@gmail.com>
2022-09-07 15:21:51 +0200

    Simple Simulink Driver implemented for Adafruit Motorshield V2 Library

---
+++ AFMSV2_Driver/src/AFMSV2Driver.cpp
@@ -0,0 +1,59 @@
+//AFMSV2Driver.cpp
+#include <math.h>
+#include "AFMSV2Driver.h" 
+#include "Adafruit_MotorShield.h"   
+
+/*Create object of the motor driver*/
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *M1 = AFMS.getMotor(1);
+Adafruit_DCMotor *M2 = AFMS.getMotor(2);
+Adafruit_DCMotor *M3 = AFMS.getMotor(3);
+Adafruit_DCMotor *M4 = AFMS.getMotor(4);
+
+uint8_t init_f;
+extern "C" void AFMSV2Driver_Init(void)
+{ 
+    // Initializing the device driver
+    if (!AFMS.begin()) init_f = 0; // Error, device not found
+    else init_f = 1; // Device found
+} 
+extern "C" void AFMSV2Driver_Step(float U1, float U2, float U3, float U4) 
+{ 
+    if(init_f) {             /* If device is initialized properly, else return 0 */
+        if (U1 > 0) M1->run(FORWARD);
+        else if (U1 < 0) {
+            M1->run(BACKWARD);
+            U1 = fabs(U1);
+        } else M1->run(RELEASE);
+        if (U1 > 4095) U1 = 4095;
+
+        if (U2 > 0) M2->run(FORWARD);
+        else if (U2 < 0) {
+            M2->run(BACKWARD);
+            U2 = fabs(U2);
+        } else M2->run(RELEASE);
+        if (U2 > 4095) U2 = 4095;
+
+        if (U3 > 0) M3->run(FORWARD);
+        else if (U3 < 0) {
+            M3->run(BACKWARD);
+            U3 = fabs(U3);
+        } else M3->run(RELEASE);
+        if (U3 > 4095) U3 = 4095;
+
+        if (U4 > 0) M4->run(FORWARD);
+        else if (U4 < 0) {
+            M4->run(BACKWARD);
+            U4 = fabs(U4);
+        } else M4->run(RELEASE);
+        if (U4 > 4095) U4 = 4095;
+
+        M1->setSpeedFine((uint16_t) U1); // 0 (full off) to 4095 (full on)
+        M2->setSpeedFine((uint16_t) U2); // 0 (full off) to 4095 (full on)
+        M3->setSpeedFine((uint16_t) U3); // 0 (full off) to 4095 (full on)
+        M4->setSpeedFine((uint16_t) U4); // 0 (full off) to 4095 (full on)
+    } 
+} 
+extern "C" void AFMSV2Driver_Terminate() 
+{ 
+} 
